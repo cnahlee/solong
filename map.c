45490bb82eb4fa3af25483158a078beccd5460a7
cnahlee <cnahle@c1r6s7.42beirut.com>
2024-09-06 20:06:53 +0300

    updateee

---
+++ map.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnahle <cnahle@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/06 16:59:29 by cnahle            #+#    #+#             */
+/*   Updated: 2024/09/06 19:37:34 by cnahle           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "solong.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+t_map	create_map(char *file_path)
+{
+	int		fd;
+	char	*line;
+	t_map	mapc;
+
+	fd = open(file_path, O_RDONLY);
+	if (fd < 0)
+		perror("can't open file");
+	mapc.longueur = 0;
+	mapc.map = malloc(sizeof(char *) * 100); // Preallocate for 100 rows (adjust as needed)
+
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		mapc.map[mapc.longueur] = line; // Store the line in the map
+		mapc.longueur++;
+	}
+
+	close(fd);
+
+	if (mapc.longueur > 0)
+		mapc.largeur = ft_strlen(mapc.map[0]); // Set map width based on first row
+	return (mapc);
+}
+#include "solong.h"
+#include <stdio.h>
+
+int	main(int argc, char **argv)
+{
+	t_map	mapc;
+	int		i;
+	(void) argc;
+
+	
+	mapc = create_map(argv[1]);
+
+	// Print map to verify
+	i = 0;
+	printf("Map Size: %d x %d\n", mapc.longueur, mapc.largeur);
+	while (i < mapc.longueur)
+	{
+		printf("%s", mapc.map[i]); // Note: No newline needed since lines already have '\n'
+		i++;
+	}
+
+	// Free allocated memory
+	i = 0;
+	while (i < mapc.longueur)
+	{
+		free(mapc.map[i]);
+		i++;
+	}
+	free(mapc.map);
+
+	return (0);
+}
---
+++ solong.h
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   solong.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnahle <cnahle@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/06 16:55:52 by cnahle            #+#    #+#             */
+/*   Updated: 2024/09/06 19:39:02 by cnahle           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SOLONG_H
+# define SOLONG_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1020
+# endif
+
+# include<stdlib.h>
+# include<stddef.h>
+# include<unistd.h>
+# include <fcntl.h>
+# include <errno.h>
+# include "mlx/mlx.h"
+
+typedef struct s_map
+{
+	int		longueur;
+	int		largeur;
+	char	**map;
+}		t_map;
+
+typedef struct s_game {
+	void	*mlx;
+	void	*win;
+	t_map	map;
+	int		player;
+	int		collectibles;
+	int		moves;
+	void	*textures[5];
+}	t_game;
+
+int		ft_strlen(char *str);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_join(char *dest, char *s1, char *s2);
+char	*ft_readed_line(char *start);
+char	*ft_move_start(char	*start);
+char	*get_next_line(int fd);
+
+#endif
